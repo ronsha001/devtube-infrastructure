2eed094340ed40763016163d9d170b8421f19054
Ron Sharabi <ronsh0111@gmail.com>
2022-12-20 17:24:42 +0200

    Replaced Jenkins VM with Github Actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,29 +33,30 @@ module "cluster-module" {
   ingress_nginx_version     = "4.4.0"
   acr_scope                 = module.acr.acr-scope
 }
-module "vm-module" {
-  source          = "./vm-module"
-  project         = var.project
-  creation_date   = var.creation_date
-  expiration_date = var.expiration_date
-  created_by      = var.created_by
-  made_by         = var.made_by
+# Replaced Jenkins VM with Github Actions
+# module "vm-module" {
+#   source          = "./vm-module"
+#   project         = var.project
+#   creation_date   = var.creation_date
+#   expiration_date = var.expiration_date
+#   created_by      = var.created_by
+#   made_by         = var.made_by
 
-  rg_location                   = "East US"
-  address_space                 = ["10.0.0.0/16"]
-  address_prefixes              = ["10.0.2.0/24"]
-  public_ip_allocation_method   = "Static"
-  private_ip_address_allocation = "Dynamic"
-  vm_size                       = "Standard_B2s"
-  admin_username                = "adminuser"
-  custom_data                   = "/home/ron/Desktop/DevTube/devtube-infrastructure/terraform/vm-module/cloud-init-jenkins.txt"
-  admin_ssh_key_username        = "adminuser"
-  public_key_path               = "~/.ssh/id_rsa.pub"
-  os_disk_chaching              = "ReadWrite"
-  storage_account_type          = "Standard_LRS"
-  publisher                     = "Canonical"
-  offer                         = "UbuntuServer"
-  sku                           = "18.04-LTS"
-  image_version                 = "latest"
-  acr_scope                     = module.acr.acr-scope
-}
+#   rg_location                   = "East US"
+#   address_space                 = ["10.0.0.0/16"]
+#   address_prefixes              = ["10.0.2.0/24"]
+#   public_ip_allocation_method   = "Static"
+#   private_ip_address_allocation = "Dynamic"
+#   vm_size                       = "Standard_B2s"
+#   admin_username                = "adminuser"
+#   custom_data                   = "/home/ron/Desktop/DevTube/devtube-infrastructure/terraform/vm-module/cloud-init-jenkins.txt"
+#   admin_ssh_key_username        = "adminuser"
+#   public_key_path               = "~/.ssh/id_rsa.pub"
+#   os_disk_chaching              = "ReadWrite"
+#   storage_account_type          = "Standard_LRS"
+#   publisher                     = "Canonical"
+#   offer                         = "UbuntuServer"
+#   sku                           = "18.04-LTS"
+#   image_version                 = "latest"
+#   acr_scope                     = module.acr.acr-scope
+# }
