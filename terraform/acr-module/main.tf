fe7893d4caceed23ae2cef3cc6749bcbed2e445f
Ron Sharabi <ronsh0111@gmail.com>
2022-12-21 13:21:24 +0200

    enabled acr admin

--- terraform/acr-module/main.tf
+++ terraform/acr-module/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.devtube.name
   location            = azurerm_resource_group.devtube.location
   sku                 = var.sku
-  admin_enabled       = false
+  admin_enabled       = true
   georeplications {
     location                = var.georeplications[0]
     zone_redundancy_enabled = true
